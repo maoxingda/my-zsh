6503caae5b2d2bd944faa98aa7a7992950bce907
maoxd <maoxingda@meican.com>
2021-09-02 15:47:01 +0800

    Add dependencies

--- cron-backup-zshrc.sh
+++ cron-backup-zshrc.sh
@@ -6,8 +6,8 @@ cp -f ~/.zshrc ${repo}/.zshrc
 
 pushd "${repo}" || exit 1
 
-git add ${repo}/.zshrc
-git commit -m "daily backup ~/.zshrc"
+git add ${repo}/.zshrc && \
+git commit -m "daily backup ~/.zshrc" && \
 git push origin master
 
 popd || exit 1
